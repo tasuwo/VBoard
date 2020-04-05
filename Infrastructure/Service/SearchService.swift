64b8915689b7cd6e97454d2e5032259e1b00c168
tasuwo <kamuhata.you@gmail.com>
2020-04-06 00:29:04 +0900

    :hammer: Refactor.

--- Infrastructure/Service/SearchService.swift
+++ Infrastructure/Service/SearchService.swift
@@ -8,8 +8,12 @@ import RxRelay
 import RxSwift
 
 public class SearchService: SearchServiceProtocol {
+    // MARK: - SearchServiceProtocol
+
     public var state: BehaviorRelay<SearchServiceQuery> = BehaviorRelay(value: .notLoaded)
     public var command: PublishRelay<SearchServiceCommand> = PublishRelay()
+
+    // MARK: - Properties
 
     private let disposeBag = DisposeBag()
     private let queue = DispatchQueue(label: "net.tasuwo.Infrastructure.SearchService")
@@ -19,12 +23,12 @@ public class SearchService: SearchServiceProtocol {
     public init() {
         let searchState = command
             .observeOn(ConcurrentDispatchQueueScheduler(queue: queue))
-            .filter { if case .search(_) = $0 { return true } else { return false } }
+            .filter { $0.isSearch }
             .map { _ in SearchServiceQuery.progress }
 
         let searchResult = command
             .observeOn(ConcurrentDispatchQueueScheduler(queue: queue))
-            .filter { if case .search(_) = $0 { return true } else { return false } }
+            .filter { $0.isSearch }
             .flatMapLatest { command -> Observable<YouTube.SearchApiResponse> in
                 switch command {
                 case let .search(query: query):
@@ -34,18 +38,28 @@ public class SearchService: SearchServiceProtocol {
                     return .never()
                 }
             }
-        .map { response -> SearchServiceQuery in
-            let items = response.items.map { $0.convertToDomainModel() }
-            let pageInfo = PageInfo(totalResults: response.pageInfo.totalResults,
-                                    resultsPerPage: response.pageInfo.resultsPerPage,
-                                    prevPageToken: response.prevPageToken,
-                                    nextPageToken: response.nextPageToken)
-            return .content(items: items, pageInfo: pageInfo)
-        }
+            .map { response -> SearchServiceQuery in
+                let items = response.items.map { $0.convertToDomainModel() }
+                let pageInfo = PageInfo(totalResults: response.pageInfo.totalResults,
+                                        resultsPerPage: response.pageInfo.resultsPerPage,
+                                        prevPageToken: response.prevPageToken,
+                                        nextPageToken: response.nextPageToken)
+                return .content(items: items, pageInfo: pageInfo)
+            }
 
         Observable
             .merge(searchState, searchResult)
             .bind(to: self.state)
             .disposed(by: self.disposeBag)
+    }
+}
+
+private extension SearchServiceCommand {
+    var isSearch: Bool {
+        if case .search(query: _) = self {
+            return true
+        } else {
+            return false
+        }
     }
 }
---
+++ InfrastructureTests/Service/SearchServiceSpec.swift
@@ -0,0 +1,16 @@
+//
+//  Copyright © 2020 Tasuku Tozawa. All rights reserved.
+//
+
+import Nimble
+import Quick
+import RxBlocking
+import RxSwift
+import RxTest
+
+@testable import Domain
+@testable import Infrastructure
+
+class SearchServiceSpec: QuickSpec {
+    // TODO:
+}
